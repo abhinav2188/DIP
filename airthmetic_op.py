a2b1b509ea427b74f6ede5b21e40984dfb7fe3a2
abhinav2188 <abhinav.mca19.du@gmail.com>
2021-01-24 23:54:28 +0530

    arithmetic operations on binary images

--- airthmetic_op.py
+++ airthmetic_op.py
@@ -1,81 +1,85 @@
-import numpy as np;
-from PIL import Image;
+from PIL import Image
+import numpy as np
 
-'''
-description :  readImg(imgPath) returns the binary equivalent 2D matrix of the 
-			   image specified by the imgPath
-'''
-def readImgAsBinary(imgPath):
-	# open file --> convert to gray scale --> divide each pixel by 255 --> return img as binary matrix
-	img = Image.open(imgPath).convert("L").point(lambda i:i/255)
-	imgData = np.asarray(img)	
-	return imgData;
+#desc : function reads a image,convert it to GRAY SCALE specfied by 'imgPath' , return its gray scale matrix
+def readGrayImg(imgPath) :
+	img = Image.open(imgPath).convert('L')
+	data = np.asarray(img)
+	return data
 
-'''
-desc : saveImg(x,y) takes x(binary image matrix) and save it as an gray scale png image ("y".png)
-'''
-def saveImg(imgMatrix,imgPath) :
-	# binary image Matrix --> image object --> gray scale image --> save img with name "imgPath.png"
-	data = Image.fromarray((imgMatrix*255).astype(np.uint8))
-	data.save(imgPath+".png")
-	print("saved "+imgPath+".png")
-
-'''
-desc: display image matrix
-'''
-def displayMatrix(m) :
+#desc : function displays a gray scale image matrix	
+def displayGray(imgMatrix) :
 	print()
-	for r in m:
-		for c in r:
-			print(c, end=" ")
-		print()	
+	for x in imgMatrix:
+		for y in x:
+			print(f"{ y : <4}",end="")
+		print()
 	print()
 
-# not operation
-def notOperation(imgMatrix) :
-	#unify numpy function for element wise manipulation
-	mynot = np.frompyfunc(lambda a:1^a,1,1)
-	return mynot(imgMatrix)
+#desc : function saves a gray scale image with the specified path
+def saveGrayImg(imgMatrix,imgPath):
+	data = Image.fromarray(imgMatrix.astype(np.uint8))
+	data.save(imgPath)
+	print("saved "+imgPath)
+
 	
-# and operation 
-def andOperation(img1,img2) :
-	if img1.shape != img2.shape : 
-		print("error : input images have different dimensions")
-	myand = np.frompyfunc(lambda a,b : a & b ,2,1)	
-	return myand(img1,img2)	
+# arithmetic and operation 
+def aradd(img1,img2) :
+	myadd = np.frompyfunc(lambda a,b:(a+b)//2, 2,1)
+	return myadd(img1,img2)
 
-# or Operation
-def orOperation(img1,img2) :
-	if img1.shape != img2.shape : 
-		print("error : input images have different dimensions")
-	myor = np.frompyfunc(lambda a,b : a | b ,2,1)	
-	return myor(img1,img2)	
+# arithmetic substraction operation 
+def arsub(img1,img2) :
+	myadd = np.frompyfunc(lambda a,b:a-b, 2,1)
+	return myadd(img1,img2)
 
-# xor operation
-def xorOperation(img1,img2) :
-	if img1.shape != img2.shape : 
-		print("error : input images have different dimensions")
-	myxor = np.frompyfunc(lambda a,b: a^b, 2,1)
-	return myxor(img1,img2)
+def addScalar(img,factor) :
+	return img+factor
+	
+def subScalar(img,factor) :
+	return img-factor
+	
+def multiplyScalar(img,factor) :
+	return img*factor
+	
+def divideScalar(img,factor) :
+	return img/factor
 
-#-----------------main()-----------------------------#
 def main() :
-	imgMatrix1 = readImgAsBinary("img1.png")
-	imgMatrix2 = readImgAsBinary("img2.png")
-	imgMatrix3 = readImgAsBinary("img3.png")
+	img1 = readGrayImg("gray1.png")
+	img2 = readGrayImg("gray2.png")
+	img3 = np.arange(100).reshape(10,10)
+	img4 = np.linspace(0,255,100).reshape(10,10).astype(np.uint8)
+	
+	print("-----------image 1----------------")
+	print(img3)
+	print("-----------image 2-----------------")
+	print(img4)
 
-	notimg3 = notOperation(imgMatrix3)
-	img3andimg1 = andOperation(imgMatrix3,imgMatrix1)
-	img3orimg1 = orOperation(imgMatrix3,imgMatrix1)
-	img3xorimg1 = xorOperation(imgMatrix3,imgMatrix1)
+	print("-------------airthmetic addition--------------")
+	print(img4+img3)
+	print("-------------airthmetic substraction--------------")
+	print(img4-img3)
 	
-	saveImg(notimg3,"notImg3")
-	saveImg(img3andimg1,"img3andimg1")
-	saveImg(img3orimg1,"img3orimg1")
-	saveImg(img3xorimg1,"img3xorimg1")
+	'''
+	g1addg2 = np.add(img1,img2)
+	saveGrayImg(g1addg2,"g1addg2.png")
+	g1subg2 = arsub(img1,img2)
+	#displayGray(g1subg2)
+	saveGrayImg(g1subg2,"g1subg2.png")
 	
+	g1scalarAdd = addScalar(img1,30);
+	#displayGray(g1scalarAdd)
+	saveGrayImg(g1scalarAdd,"g1scalarAdd.png")
+	'''
+	print("-------------scalar addition--------------")
+	print(img3+10)
+	print("-------------scalar substraction--------------")
+	print(img3-10)
+	print("-------------scalar multiplication--------------")
+	print(img3*10)
+	print("-------------scalar division--------------")
+	print((img3/10).astype(np.uint8))
+
 if __name__ == "__main__" :
 	main()
-	
-	
-	
